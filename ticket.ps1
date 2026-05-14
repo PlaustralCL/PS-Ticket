270eec28da1dd1ea3f6d5c06dd8f18d24bbd2c58
PlaustralCL <plaustralcl@gmail.com>
2026-05-14 10:52:53 -0400

    Add notes

--- ticket.ps1
+++ ticket.ps1
@@ -1,31 +1,30 @@
-﻿# Configuration
-$ticketsDirectory = "1-tickets"
-$templatesDirectory = "templates"
+﻿# Create a uniquely named folder to simplify keeping related documents together.
+# Different types of tickets are offered. Each type has a different
+# template that determines what is initially populated in the new folder.
+# There is also a misc template that is copied to every new ticket folder.
+#
+# The types of new tickets that are offered is determined by the names
+# of the directories in the templates folder. Only types/templates that are
+# ready to be used should be listed in the template folder. Draft templates
+# should be kept in a separate folder.
+#
+# There is no "other" type. Instead, the "misc" type based on the "misc"
+# template folder name is used. Some branching is hard coded based on a 
+# ticket type of "misc". Removing the misc template folder/ type option 
+# without updating the code will result in errors.
 
-#$ticketTypes = @(
-#    "pvsr"
-#    "cmpl"
-#    "other"
-#)
-
-
-
-# Application code
 $originalDirectory = Get-Location
 
 # Set location to the script root. This will make relative paths easier.
 Set-Location $PSScriptRoot
-
 
 # Use the directory names in the templates directory as the source
 # of ticket types. The names are converted to strings to make them easier
 # work with.
 $ticketTypes = [string []] (Get-ChildItem -Path $templatesDirectory -Directory)
 
-
-
-
-
+$ticketsDirectory = "1-tickets"
+$templatesDirectory = "templates"
 
 While ($true) {
     Clear-Host
@@ -61,24 +60,21 @@ While ($true) {
         exit
     }
     
-    $ticketType = $ticketTypes[[int] $userInput]
-    $flair = ""    
-
-    if ($ticketType -ne "misc") { 
-        $flair = $ticketType.ToUpper()
-        $templatePath = Join-Path -Path $templatesDirectory -ChildPath $ticketType
-    }  
-
-
+    #Convert the number input to a string that decribes the type of ticket
+    $ticketType = $ticketTypes[[int] $userInput]      
+    
     # Build ticket name
     $index = [int] (Get-Content -Path "index.txt")
     $twoDigitYear = Get-Date -Format "yy"
-    $ticketName = ""
-    if ($flair -eq "") {
-        $ticketName = "$twoDigitYear.$index - $ticketTopic"
-    } else {
+    $ticketName = "" 
+
+    if ($ticketType -ne "misc") { 
+        $flair = $ticketType.ToUpper()
         $ticketName = "$twoDigitYear.$index.$flair - $ticketTopic"
-    } 
+        $templatePath = Join-Path -Path $templatesDirectory -ChildPath $ticketType
+    } else {
+        $ticketName = "$twoDigitYear.$index - $ticketTopic"
+    }
 
     $ticketPath = Join-Path -Path $ticketsDirectory -ChildPath $ticketName
 
@@ -107,6 +103,7 @@ While ($true) {
     Write-Host "The ticket name has been copied to the clipboard"
     Read-Host "Press <enter> to continue..."
     Write-Host ""
+
     Write-Host "Path to new folder:"
     Write-Host $(Resolve-Path $ticketPath)
     Set-Clipboard $(Resolve-Path $ticketPath)
@@ -114,5 +111,5 @@ While ($true) {
     Read-Host "Press <enter> to continue..."
 }
 
-# Return the shell to the original condition after exiting.
+# Return the shell to the original location before exiting.
 Set-Location $originalDirectory
